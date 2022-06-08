bc32c25b89ceac21d9177bb1a53f85d6b4801b4b
Lisa Meyer <lisamermey@gmail.com>
2022-06-08 11:34:35 -0400

    Import core grid css and theme

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
 import React, { useState, useRef, useEffect, useMemo, useCallback } from 'react';
 import { render } from 'react-dom';
 import { AgGridReact } from 'ag-grid-react';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
