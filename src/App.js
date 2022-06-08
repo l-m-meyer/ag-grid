becbc4460563b2a27a029ca4b53aae205f7fa349
Lisa Meyer <lisamermey@gmail.com>
2022-06-08 11:39:20 -0400

    Add grid reference and state for rows and columns

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React, { useState, useRef, useEffect, useMemo, useCallback } from 'react'
 import { render } from 'react-dom';
 import { AgGridReact } from 'ag-grid-react';
 
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import 'ag-grid-community/dist/styles/ag-grid.css'; // Core grid CSS, always needed
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css'; // Optional theme CSS
+
+const App = () => {
+
+  const gridRef = useRef(); // Optional - for accessing Grid's API
+  const [rowData, setRowData] = useState(); // Set rowData to Array of Objects, one Object per Row
+
+  // Each Column Definition results in one Column
+  const [columnDefs, setColumnDefs] = useState([
+    { field: 'make', filter: true },
+    { field: 'model', filter: true },
+    { field: 'price' }
+  ]);
+}
